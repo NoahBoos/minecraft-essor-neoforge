6e4ffc526857ba10290dfa2e97c5bfe3cdc8fd7d
Noah Boos <nboosgamemaker@gmail.com>
2025-11-12 17:54:12 +0100

    Feat. - Implemented the `OnItemTooltip()` event handler.

--- src/main/java/fr/noahboos/essor/EssorEventHandler.java
+++ src/main/java/fr/noahboos/essor/EssorEventHandler.java
@@ -1,11 +1,15 @@
 package fr.noahboos.essor;
 
-import fr.noahboos.essor.components.EquipmentLevelingData;
-import fr.noahboos.essor.components.EssorDataComponents;
+import fr.noahboos.essor.component.EquipmentLevelingData;
+import fr.noahboos.essor.component.EssorDataComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+
+import java.util.List;
 
 @EventBusSubscriber
 public class EssorEventHandler {
@@ -27,4 +31,31 @@ public class EssorEventHandler {
         }
     }
 
+    @SubscribeEvent
+    public static void OnItemTooltip(ItemTooltipEvent event) {
+        List<Component> tooltip = event.getToolTip();
+        ItemStack item = event.getItemStack();
+        if (item.getComponents().has(EssorDataComponents.EQUIPMENT_LEVELING_DATA.get())) {
+            EquipmentLevelingData data = item.getComponents().get(EssorDataComponents.EQUIPMENT_LEVELING_DATA.get());
+
+            StringBuilder prestigeProgressBar = new StringBuilder();
+            int prestigeSegments = 10;
+            int prestigeFilledSegments = data.GetPrestige();
+            prestigeProgressBar.append("§6★".repeat(Math.max(0, prestigeFilledSegments)));
+            prestigeProgressBar.append("§7☆".repeat(Math.max(0, prestigeSegments - prestigeFilledSegments)));
+            tooltip.add(Component.empty());
+            tooltip.add(Component.literal(Component.translatable("tooltip.essor.prestige", data.GetPrestige()).getString() + " " + prestigeProgressBar.toString()));
+            tooltip.add(Component.translatable("tooltip.essor.totalExperienceMultiplier", data.GetTotalExperienceMultiplier()));
+            tooltip.add(Component.translatable("tooltip.essor.prestigeExperienceMultiplier", data.GetPrestigeExperienceMultiplier()));
+            tooltip.add(Component.translatable("tooltip.essor.challengeExperienceMultiplier", data.GetChallengeExperienceMultiplier()));
+            tooltip.add(Component.empty());
+            tooltip.add(Component.translatable("tooltip.essor.level", data.GetLevel(), data.GetCurrentExperience(), data.GetRequiredExperienceToLevelUp()));
+            StringBuilder levelProgressBar = new StringBuilder();
+            int levelSegments = 25;
+            int levelFilledSegments = (int) (((float) data.GetCurrentExperience() / (float) data.GetRequiredExperienceToLevelUp()) * levelSegments);
+            levelProgressBar.append("§a■".repeat(Math.max(0, levelFilledSegments)));
+            levelProgressBar.append("§7□".repeat(Math.max(0, levelSegments - levelFilledSegments)));
+            tooltip.add(Component.literal(levelProgressBar.toString()));
+        }
+    }
 }
