3b060c41d2344a1ac8c30457a36c65d3dd2a150d
Noah Boos <nboosgamemaker@gmail.com>
2025-11-12 12:04:12 +0100

    Feat. - Registered the `EssorEventHandler` class.

--- src/main/java/fr/noahboos/essor/Essor.java
+++ src/main/java/fr/noahboos/essor/Essor.java
@@ -6,6 +6,7 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
@@ -21,9 +22,8 @@ public class Essor
     public Essor(IEventBus modEventBus, ModContainer modContainer)
     {
         modEventBus.addListener(this::commonSetup);
-
         EssorDataComponents.REGISTRAR.register(modEventBus);
-
+        NeoForge.EVENT_BUS.register(EssorEventHandler.class);
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
