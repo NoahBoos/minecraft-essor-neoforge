dfb259f5c237d2244e3abdc1fc617f1322a14b1d
Noah Boos <nboosgamemaker@gmail.com>
2025-11-12 15:05:38 +0100

    Feat. - Copied the JsonLoader, but disabled methods until they are necessary.

---
+++ src/main/java/fr/noahboos/essor/utils/JsonLoader.java
@@ -0,0 +1,42 @@
+package fr.noahboos.essor.utils;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class JsonLoader {
+//    public static Map<String, Float> LoadExperienceData(String path) {
+//        try (InputStream stream = JsonLoader.class.getClassLoader().getResourceAsStream(path)) {
+//            if (stream == null) {
+//                throw new  RuntimeException("Failed to load experience data from " + path);
+//            }
+//            Reader reader = new InputStreamReader(stream);
+//            Gson gson = new Gson();
+//            Type type = new TypeToken<Map<String, Float>>(){}.getType();
+//            return gson.fromJson(reader, type);
+//        } catch (Exception exception) {
+//            exception.printStackTrace();
+//            return null;
+//        }
+//    }
+//
+//    public static Map<Integer, Map<String, Integer>> LoadRewardData(String path) {
+//        try (InputStream stream = JsonLoader.class.getClassLoader().getResourceAsStream(path)) {
+//            if (stream == null) {
+//                throw new  RuntimeException("Failed to load reward data from " + path);
+//            }
+//            Reader reader = new InputStreamReader(stream);
+//            Gson gson = new Gson();
+//            Type type = new TypeToken<Map<Integer, Map<String, Integer>>>(){}.getType();
+//            return gson.fromJson(reader, type);
+//        } catch (Exception exception) {
+//            exception.printStackTrace();
+//            return null;
+//        }
+//    }
+}
