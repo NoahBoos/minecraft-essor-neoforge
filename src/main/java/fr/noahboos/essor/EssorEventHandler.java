33099da9e8312131cf0a26009fc19273eea8753c
Noah Boos <nboosgamemaker@gmail.com>
2025-11-12 10:40:36 +0100

    Feat. - Implemented the `EssorEventHandler` class.

---
+++ src/main/java/fr/noahboos/essor/EssorEventHandler.java
@@ -0,0 +1,10 @@
+package fr.noahboos.essor;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+
+@EventBusSubscriber
+public class EssorEventHandler {
+
+}
