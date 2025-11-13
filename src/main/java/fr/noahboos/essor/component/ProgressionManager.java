e223c243ffb98373660a1d3bcbde4f50454309f2
Noah Boos <nboosgamemaker@gmail.com>
2025-11-13 17:47:17 +0100

    Feat. - Implemented the `AddExperience()` method in the `ProgressionManager` class.

--- src/main/java/fr/noahboos/essor/component/ProgressionManager.java
+++ src/main/java/fr/noahboos/essor/component/ProgressionManager.java
@@ -3,5 +3,9 @@ package fr.noahboos.essor.component;
 import net.minecraft.world.item.ItemStack;
 
 public class ProgressionManager {
-
+    public static void AddExperience(ItemStack item, float experience) {
+        EquipmentLevelingData data = item.getComponents().get(EssorDataComponents.EQUIPMENT_LEVELING_DATA.get());
+        if (data == null) return;
+        data.SetCurrentExperience((float) Math.round((data.GetCurrentExperience() + experience) * 1000f) / 1000f);
+    }
 }
