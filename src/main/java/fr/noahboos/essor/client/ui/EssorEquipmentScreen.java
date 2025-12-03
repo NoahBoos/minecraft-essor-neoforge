962c8fd6b18c2095b784e591837019928e553cbe
Noah Boos <nboosgamemaker@gmail.com>
2025-12-03 10:49:52 +0100

    Feat. - Added a custom width and a custom height to the `EssorEquipmentScreen`.

--- src/main/java/fr/noahboos/essor/client/ui/EssorEquipmentScreen.java
+++ src/main/java/fr/noahboos/essor/client/ui/EssorEquipmentScreen.java
@@ -5,18 +5,26 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 public class EssorEquipmentScreen extends Screen {
+    private int panelWidth;
+    private int panelHeight;
+    private int panelLeft;
+    private int panelTop;
+
     public EssorEquipmentScreen() {
         super(Component.literal("Essor - Equipment panel"));
     }
 
     @Override
     protected void init() {
-
+        this.panelWidth = (int) Math.round(this.width * 0.6);
+        this.panelHeight = (int) Math.round(this.height * 0.6);
+        this.panelLeft = (this.width - this.panelWidth) / 2;
+        this.panelTop = (this.height - this.panelHeight) / 2;
     }
 
     @Override
     public void renderBackground(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-        graphics.fill(0, 0, this.width, this.height, 0xFF202020);
+        graphics.fill(this.panelLeft, this.panelTop, this.panelLeft + this.panelWidth, this.panelTop + this.panelHeight, 0xFF202020);
     }
 
     @Override
@@ -26,8 +34,8 @@ public class EssorEquipmentScreen extends Screen {
         graphics.drawString(
                 this.font,
                 "Essor - Equipment panel",
-                this.width / 2 - this.font.width("Essor - Equipment panel") / 2,
-                20,
+                this.panelLeft + this.panelWidth / 2 - this.font.width("Essor - Equipment panel") / 2,
+                this.panelTop + 48,
                 0xFFFFE01B
         );
 
