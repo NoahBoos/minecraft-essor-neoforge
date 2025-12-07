bf9bd883207a29a92ef14bf771039e102870b584
Noah Boos <nboosgamemaker@gmail.com>
2025-12-07 15:02:55 +0100

    Feat. - Added a challenge icon handling system to `ChallengeDefinition`.

--- src/main/java/fr/noahboos/essor/component/challenge/ChallengeDefinition.java
+++ src/main/java/fr/noahboos/essor/component/challenge/ChallengeDefinition.java
@@ -1,11 +1,15 @@
 package fr.noahboos.essor.component.challenge;
 
+import fr.noahboos.essor.Essor;
 import fr.noahboos.essor.util.JsonLoader;
+import net.minecraft.resources.ResourceLocation;
 
 import java.util.List;
 
 public class ChallengeDefinition {
     private String id;
+    private String iconId;
+    private ResourceLocation icon;
     private int maximumTier;
     private List<Integer> thresholds;
     private List<String> targets;
@@ -19,6 +23,11 @@ public class ChallengeDefinition {
         }
 
         this.id = buffer.id;
+        if (buffer.iconId != null) {
+            this.icon = ResourceLocation.fromNamespaceAndPath(Essor.MODID, buffer.iconId);
+        } else {
+            System.err.println("Challenge definition " + this.id + " has no icon defined");
+        }
         this.maximumTier = buffer.maximumTier;
         this.thresholds = buffer.thresholds;
         this.targets = buffer.targets;
@@ -26,6 +35,10 @@ public class ChallengeDefinition {
 
     public String GetId() {
         return id;
+    }
+
+    public ResourceLocation GetIcon() {
+        return icon;
     }
 
     public int GetMaximumTier() {
